49ed68d8cf373978f8aa22d3e952e8415fd9b500
Stepan <ssypachev@dynamixsoftware.com>
2019-10-03 16:34:37 +0500

    ru_colored_animals and tests for vocabulary

--- tests/vocabularies.js
+++ tests/vocabularies.js
@@ -1,13 +1,26 @@
 const   chai    = require('chai'),
       { NVRNG } = require('../index.js');
 	  
-const filename  = __dirname + '/../vocabularies/en_colored_animals.json';
+const filename_en  = __dirname + '/../vocabularies/en_colored_animals.json';
+const filename_ru  = __dirname + '/../vocabularies/ru_colored_animals.json';
 
 describe('Should test ready-to-use vocabularies', () => {
 	
 	it ('Should test en_colored_animals.json', () => {
 		let gen = new NVRNG();
-		let uerr = gen.upload(filename);
+		let uerr = gen.upload(filename_en);
+		chai.expect(uerr).to.be.null;
+		
+		let [err, out] = gen.getSet(30);
+		chai.expect(err).to.be.null;
+		chai.expect(out).not.to.be.null;
+		chai.expect(out.size).to.equal(30);
+		out.forEach((item) => console.log(item));
+	});
+	
+	it ('Should test en_colored_animals.json', () => {
+		let gen = new NVRNG();
+		let uerr = gen.upload(filename_ru);
 		chai.expect(uerr).to.be.null;
 		
 		let [err, out] = gen.getSet(30);
