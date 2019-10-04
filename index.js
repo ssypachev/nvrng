f79088ab19e640beae964c808fd0331198ef9fd2
Stepan <ssypachev@dynamixsoftware.com>
2019-10-04 13:23:10 +0500

    clean tabs

--- index.js
+++ index.js
@@ -1,23 +1,23 @@
 let fs = require('fs');
 
 let Genders = {
-	Any:     "a",
-	Female:  "f",
-	Male:    "m",
-	Neutral: "n"
+    Any:     "a",
+    Female:  "f",
+    Male:    "m",
+    Neutral: "n"
 };
 
 let OutputFormat = {
-	Set:    "set",
-	Array:  "array",
-	Object: "object"
+    Set:    "set",
+    Array:  "array",
+    Object: "object"
 };
 
 let StringFormat = {
-	Lowercase:  "lowercase",
-	Uppercase:  "uppercase",
-	Capitalize: "capitalize",
-	NoFormat:   "noformat"
+    Lowercase:  "lowercase",
+    Uppercase:  "uppercase",
+    Capitalize: "capitalize",
+    NoFormat:   "noformat"
 };
 
 let lowerCaseFormatter  = str => str.toLowerCase();
@@ -26,206 +26,206 @@ let capitalizeFormatter = str => str.charAt(0).toUpperCase() + str.slice(1).toLo
 let noFormatFormatter   = str => str;
 
 let addToSet = (set, val, _) => {
-	set.add(val);
+    set.add(val);
 }
 let addToSetAndExclude = (set, val, exclude) => {
-	if (!exclude.has(val)) {
-		set.add(val);
-	}
+    if (!exclude.has(val)) {
+        set.add(val);
+    }
 }
 
 class NVRNG {
-	/*
-	this.rset        - data itself
-	this.genders     - set of all different genders
-	this.spaces      - array of keys
-	this.spaceLength - length of spaces array
-	*/
-	
-	constructor ({ limit = 100000 } = {}) {
-		this.limit = limit;
-	}
-	
-	static getStringFormatter (format) {
-		switch (format) {
-		case StringFormat.Lowercase:  return lowerCaseFormatter;
-		case StringFormat.Uppercase:  return upperCaseFormatter;
-		case StringFormat.Capitalize: return capitalizeFormatter;
-		case StringFormat.NoFormat:   return noFormatFormatter;
-		default: return null;
-		}
-	}
-	
-	static getAdders () {
-		return [addToSet, addToSetAndExclude];
-	}
-	
-	static randIntFromZero (max) {
-		//does not reach max
-		return Math.floor(Math.random() * (max));
-	}
-	
-	static shuffle (arr) {
-		let i = arr.length - 1;
-		for (; i > 0; i -= 1) {
-			let j = NVRNG.randIntFromZero(i),
-			    tmp = arr[j];
-			arr[j] = arr[i];
-			arr[i] = tmp;
-		}
-	}
-	
-	getData () {
-		return this.rset;
-	}
-	
-	upload (filename, { shuffle = true } = {}) {
-		let self = this;
-		try {
-			self.rset = require(filename);
-		} catch (err) {
-			return err;
-		}
-		if (shuffle) {
-			for (let [key, space] of Object.entries(self.rset)) {
-				for (let [key, arr] of Object.entries(space)) {
-					NVRNG.shuffle(arr);
-				}
-			}
-		}
-		let allKeySets = [];
-		for (let [key, space] of Object.entries(self.rset)) {
-			allKeySets.push(Object.keys(space).sort());
-		}
-		self.spaces = Object.keys(self.rset);
-		self.spaceLength = self.spaces.length;
-		
-		for (let i = 0; i < self.spaceLength - 1; i += 1) {
-			if (JSON.stringify(allKeySets[i]) !== JSON.stringify(allKeySets[i+1])) {
-				return `Key sets [${allKeySets[i]}] of "${self.spaces[i]}" and [${allKeySets[i+1]}] of "${self.spaces[i+1]}" are not equal`;
-			}
-		}
-		
-		self.keys = JSON.parse(JSON.stringify(allKeySets[0]));
-		allKeySets = [];
-		return null;
-	}
-	
-	getArrOfGender (space, gender) {
-		let self = this;
-		if (gender === Genders.Any) {
-			let proGender = self.keys[ NVRNG.randIntFromZero(self.keys.length) ];
-			return [proGender, space[proGender]]; 
-		}
-		return [gender, space[gender]];
-	}
-	
-	static convertSet (data, type = OutputFormat.Set) {
-		switch (type) {
-			case OutputFormat.Set:    
-				return data;
-			case OutputFormat.Array:
-				return [...data];
-			case OutputFormat.Object: {
-				let out = {};
-				data.forEach((item) => {
-					out[item] = true;
-				});
-				return out;
-			}
-			default:
-				throw `Convert type "${type}" is not supported`;
-		}
-	}
-	
-	static arrOrSetToSet (arr) {
-		if (Array.isArray(arr)) {
-			return [null, new Set(arr)];
-		}
-		if (! (arr instanceof Set)) {
-			return [`option include must be of type Array or Set`, null];
-		}
-		return [null, arr];
-	}
-	
-	getKeys () {
-		return this.keys;
-	}
-	
-	getOne ({ gender = Genders.Any, output = OutputFormat.Set, exclude = new Set(), delimiter = ' ', format = StringFormat.NoFormat } = {}) {
-		let self = this;
-		if (gender !== Genders.Any) {
-			if (!self.keys.includes(gender)) {
-				return [`Vocabulary does not contain arrays of ${gender} gender, supported only ${self.keys}`, null];
-			}
-		}
-		let [err, proExclude] = NVRNG.arrOrSetToSet(exclude);
-		if (err) {
-			return [err, null];
-		}
-		let formatter = NVRNG.getStringFormatter(format);
-		if (formatter === null) {
-			return [`Unsupported output string format "${format}". Use [${Object.keys(StringFormat)}]`, null];
-		}
-		let limitter = self.limit,
-			out;
-		do {
-			let p = [], arr;
-			let proGender = gender;
-			for (let space of self.spaces) {
-				[proGender, arr] = self.getArrOfGender(self.rset[space], proGender);
-				let index = NVRNG.randIntFromZero(arr.length);
-				p.push(formatter(arr[index]));
-			}
-			out = p.join(delimiter);
-		} while (limitter-->0 &&
-			proExclude.has(out));
-		return [null, out];
-	}
-	
-	getSet (size, { gender = Genders.Any, output = OutputFormat.Set, include = new Set(), exclude = new Set(), delimiter = ' ', format = StringFormat.NoFormat } = {}) {
-		let self = this;
-		if (gender !== Genders.Any) {
-			if (!self.keys.includes(gender)) {
-				return [`Vocabulary does not contain arrays of ${gender} gender, supported only ${self.keys}`, null];
-			}
-		}
-		let err, proInclude, proExclude;
-		[err, proInclude] = NVRNG.arrOrSetToSet(include);
-		if (err) {
-			return [err, null];
-		}
-		let out = new Set(proInclude);
-		size += proInclude.size;
-		[err, proExclude] = NVRNG.arrOrSetToSet(exclude);
-		if (err) {
-			return [err, null];
-		}
-		let formatter = NVRNG.getStringFormatter(format);
-		if (formatter === null) {
-			return [`Unsupported output string format "${format}". Use [${Object.keys(StringFormat)}]`, null];
-		}
-		let adder     = proExclude.size > 0 ? addToSetAndExclude : addToSet,
-			limitter  = self.limit;
-		while (out.size < size && limitter-->0) {
-			let p = [], arr;
-			let proGender = gender;
-			for (let space of self.spaces) {
-				[proGender, arr] = self.getArrOfGender(self.rset[space], proGender);
-				let index = NVRNG.randIntFromZero(arr.length);
-				p.push(formatter(arr[index]));
-			}
-			let tmp = p.join(delimiter);
-			adder(out, tmp, proExclude);
-		}
-		switch (output) {
-			case OutputFormat.Set: case OutputFormat.Array: case OutputFormat.Object: 
-				break;
-			default: 
-				return [`Convert type "${output}" is not supported`, out];
-		}
-		return [null, NVRNG.convertSet(out, output)];
-	}
+    /*
+    this.rset        - data itself
+    this.genders     - set of all different genders
+    this.spaces      - names of keys in rset
+    this.spaceLength - length of spaces array
+    */
+
+    constructor ({ limit = 100000 } = {}) {
+        this.limit = limit;
+    }
+
+    static getStringFormatter (format) {
+        switch (format) {
+        case StringFormat.Lowercase:  return lowerCaseFormatter;
+        case StringFormat.Uppercase:  return upperCaseFormatter;
+        case StringFormat.Capitalize: return capitalizeFormatter;
+        case StringFormat.NoFormat:   return noFormatFormatter;
+        default: return null;
+        }
+    }
+
+    static getAdders () {
+        return [addToSet, addToSetAndExclude];
+    }
+
+    static randIntFromZero (max) {
+        //does not reach max
+        return Math.floor(Math.random() * (max));
+    }
+
+    static shuffle (arr) {
+        let i = arr.length - 1;
+        for (; i > 0; i -= 1) {
+            let j = NVRNG.randIntFromZero(i),
+                tmp = arr[j];
+            arr[j] = arr[i];
+            arr[i] = tmp;
+        }
+    }
+
+    getData () {
+        return this.rset;
+    }
+
+    upload (filename, { shuffle = true } = {}) {
+        let self = this;
+        try {
+            self.rset = require(filename);
+        } catch (err) {
+            return err;
+        }
+        if (shuffle) {
+            for (let [key, space] of Object.entries(self.rset)) {
+                for (let [key, arr] of Object.entries(space)) {
+                    NVRNG.shuffle(arr);
+                }
+            }
+        }
+        let allKeySets = [];
+        for (let [key, space] of Object.entries(self.rset)) {
+            allKeySets.push(Object.keys(space).sort());
+        }
+        self.spaces = Object.keys(self.rset);
+        self.spaceLength = self.spaces.length;
+
+        for (let i = 0; i < self.spaceLength - 1; i += 1) {
+            if (JSON.stringify(allKeySets[i]) !== JSON.stringify(allKeySets[i+1])) {
+                return `Key sets [${allKeySets[i]}] of "${self.spaces[i]}" and [${allKeySets[i+1]}] of "${self.spaces[i+1]}" are not equal`;
+            }
+        }
+
+        self.keys = JSON.parse(JSON.stringify(allKeySets[0]));
+        allKeySets = [];
+        return null;
+    }
+
+    getArrOfGender (space, gender) {
+        let self = this;
+        if (gender === Genders.Any) {
+            let proGender = self.keys[ NVRNG.randIntFromZero(self.keys.length) ];
+            return [proGender, space[proGender]];
+        }
+        return [gender, space[gender]];
+    }
+
+    static convertSet (data, type = OutputFormat.Set) {
+        switch (type) {
+            case OutputFormat.Set:
+                return data;
+            case OutputFormat.Array:
+                return [...data];
+            case OutputFormat.Object: {
+                let out = {};
+                data.forEach((item) => {
+                    out[item] = true;
+                });
+                return out;
+            }
+            default:
+                throw `Convert type "${type}" is not supported`;
+        }
+    }
+
+    static arrOrSetToSet (arr) {
+        if (Array.isArray(arr)) {
+            return [null, new Set(arr)];
+        }
+        if (! (arr instanceof Set)) {
+            return [`option include must be of type Array or Set`, null];
+        }
+        return [null, arr];
+    }
+
+    getKeys () {
+        return this.keys;
+    }
+
+    getOne ({ gender = Genders.Any, output = OutputFormat.Set, exclude = new Set(), delimiter = ' ', format = StringFormat.NoFormat } = {}) {
+        let self = this;
+        if (gender !== Genders.Any) {
+            if (!self.keys.includes(gender)) {
+                return [`Vocabulary does not contain arrays of ${gender} gender, supported only ${self.keys}`, null];
+            }
+        }
+        let [err, proExclude] = NVRNG.arrOrSetToSet(exclude);
+        if (err) {
+            return [err, null];
+        }
+        let formatter = NVRNG.getStringFormatter(format);
+        if (formatter === null) {
+            return [`Unsupported output string format "${format}". Use [${Object.keys(StringFormat)}]`, null];
+        }
+        let limitter = self.limit,
+            out;
+        do {
+            let p = [], arr;
+            let proGender = gender;
+            for (let space of self.spaces) {
+                [proGender, arr] = self.getArrOfGender(self.rset[space], proGender);
+                let index = NVRNG.randIntFromZero(arr.length);
+                p.push(formatter(arr[index]));
+            }
+            out = p.join(delimiter);
+        } while (limitter-->0 &&
+            proExclude.has(out));
+        return [null, out];
+    }
+
+    getSet (size, { gender = Genders.Any, output = OutputFormat.Set, include = new Set(), exclude = new Set(), delimiter = ' ', format = StringFormat.NoFormat } = {}) {
+        let self = this;
+        if (gender !== Genders.Any) {
+            if (!self.keys.includes(gender)) {
+                return [`Vocabulary does not contain arrays of ${gender} gender, supported only ${self.keys}`, null];
+            }
+        }
+        let err, proInclude, proExclude;
+        [err, proInclude] = NVRNG.arrOrSetToSet(include);
+        if (err) {
+            return [err, null];
+        }
+        let out = new Set(proInclude);
+        size += proInclude.size;
+        [err, proExclude] = NVRNG.arrOrSetToSet(exclude);
+        if (err) {
+            return [err, null];
+        }
+        let formatter = NVRNG.getStringFormatter(format);
+        if (formatter === null) {
+            return [`Unsupported output string format "${format}". Use [${Object.keys(StringFormat)}]`, null];
+        }
+        let adder     = proExclude.size > 0 ? addToSetAndExclude : addToSet,
+            limitter  = self.limit;
+        while (out.size < size && limitter-->0) {
+            let p = [], arr;
+            let proGender = gender;
+            for (let space of self.spaces) {
+                [proGender, arr] = self.getArrOfGender(self.rset[space], proGender);
+                let index = NVRNG.randIntFromZero(arr.length);
+                p.push(formatter(arr[index]));
+            }
+            let tmp = p.join(delimiter);
+            adder(out, tmp, proExclude);
+        }
+        switch (output) {
+            case OutputFormat.Set: case OutputFormat.Array: case OutputFormat.Object:
+                break;
+            default:
+                return [`Convert type "${output}" is not supported`, out];
+        }
+        return [null, NVRNG.convertSet(out, output)];
+    }
 }
 
 module.exports.NVRNG   = NVRNG;
